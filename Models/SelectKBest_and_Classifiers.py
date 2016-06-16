78b0df411470ce1fd25abb84c0ad2534662001cb
Caitlin <caitlingerrity@gmail.com>
2016-06-16 17:01:32 -0400

    add binarization to models, lowering prediction threshold to improve recall

--- Models/SelectKBest_and_Classifiers.py
+++ Models/SelectKBest_and_Classifiers.py
@@ -8,9 +8,30 @@ from sklearn.cross_validation import cross_val_score, train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.feature_selection import SelectKBest
 from sklearn.linear_model import LogisticRegression
-
-# define a random state to use throughout
+from sklearn.preprocessing import binarize
+import warnings
+warnings.filterwarnings("ignore")
+# define a random state and train test split % to use throughout
 rs = 8
+tts = .2
+# define functions for classification model evaluation
+def doClassifMetrics(y_test, y_pred):
+    print 'Confusion Matrix \n', confusion_matrix(y_test, y_pred)
+    print 'Classification Report \n', classification_report(y_test, y_pred)
+
+def modelEval(name, model, X, y, binarize_threshold):
+    X_train, X_test, y_train, y_test = train_test_split(X_kbest, y, test_size=0.2,
+        stratify = y, random_state = rs)
+    meancvscore = cross_val_score(model, X, y, n_jobs=-1, verbose=1).mean()
+    print 'Model %s cross_val_score: %f' % (name, meancvscore)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    y_pred_adj = binarize(model.predict_proba(X_test)[:,1],
+        threshold = binarize_threshold, copy=False).transpose()
+    print 'Model %s classification metrics:' % name
+    doClassifMetrics(y_test, y_pred)
+    print 'Model %s using prediction threshold %f:' % (name, binarize_threshold)
+    doClassifMetrics(y_test, y_pred_adj)
 
 # import data and define X, y (features, target)
 df = pd.read_csv('../Assets/merged.csv')
@@ -22,44 +43,19 @@ y = df['WnvPresent'].astype(int)
 kbest = SelectKBest(k=10)
 X_kbest = kbest.fit_transform(X,y)
 
-# get list of best feature names
+# get list of best feature names, and put into a dataframe for reference
 best_features = [x for (x,y) in zip(X.columns, kbest.get_support().tolist()) if y==1]
-
-best_features
-# put feature names into dataframe (for reference)
 X_kbest = pd.DataFrame(X_kbest, columns = best_features)
 
 
-# do train_test_split on best features and target
-X_train, X_test, y_train, y_test = train_test_split(X_kbest, y, test_size=0.2,
-    stratify = y, random_state = rs)
-
 
 # instantiate and fit LogisticRegression
-lg = LogisticRegression(random_state=rs, n_jobs=-1, verbose=1)
+lg = LogisticRegression(random_state=rs, n_jobs=-1)
 
-# cross_val_score on all data
-print cross_val_score(lg, X_kbest, y, n_jobs=-1, verbose=1).mean()
+modelEval('LogisticRegression', lg, X_kbest, y, 0.2)
 
-# fit model on training data and look at classification metrics
-lg.fit(X_train, y_train)
-lg.score(X_test, y_test)
-lg_pred = lg.predict(X_test)
-
-print confusion_matrix(y_test, lg_pred)
-
-print classification_report(y_test, lg_pred)
 
 # instantiate  RandomForestClassifier
-rf = RandomForestClassifier(random_state=rs, n_jobs=-1, verbose=1, max_depth = None)
+rf = RandomForestClassifier(random_state=rs, n_jobs=-1, max_depth = None)
 
-# look at cross_val_score of rf on all data
-print cross_val_score(rf, X_kbest, y, n_jobs=-1, verbose=1).mean()
-
-# fit model on training data and look at classification metrics
-rf.fit(X_train, y_train)
-rf.score(X_test, y_test)
-
-rf_pred = rf.predict(X_test)
-print confusion_matrix(y_test, rf_pred)
-print classification_report(y_test, rf_pred)
+modelEval('RandomForestClassifier', rf, X_kbest, y, 0.2)
